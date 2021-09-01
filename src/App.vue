7fc2fe56b50eead96be22bf5947be8d724bc4bf2
Emilio Scodeler <emilios@ciandt.com>
2021-09-01 09:46:41 -0300

    Style gallery

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <ul>
-      <li v-for="photo of photos" :key="photo.id">
-        <img :src="photo">
+  <div class="container">
+    <h1 class="page-title">{{ title }}</h1>
+    <ul class="list-photos">
+      <li class="list-photos__item" v-for="photo of photos" :key="photo.id">
+        <p class="list-photos__item-title">{{ photo.title }}</p>
+        <img :src="photo.thumbnailUrl">
       </li>
     </ul>
   </div>
@@ -13,19 +14,16 @@
 export default {
   data() {
     return {
-      title: "Dogs",
+      title: "Gallery",
       photos: [],
     };
   },
   created() {
-    const apis = [
-      'https://dog.ceo/api/breed/hound/english/images',
-      'https://jsonplaceholder.typicode.com/photos'
-    ]
+    const api = 'https://jsonplaceholder.typicode.com/albums/1/photos'
 
-    this.$http.get(apis[0])
+    this.$http.get(api)
     .then(res => res.json())
-    .then(data => this.photos = data.message, err => {
+    .then(data => this.photos = data.slice(0, 21), err => {
       console.log(err)
     })
   }
@@ -33,4 +31,27 @@ export default {
 </script>
 
 <style>
+  .container {
+    width: 89%;
+    margin: 0 auto;
+    font-family: sans-serif;
+  }
+
+  .page-title {
+    text-align: center;
+  }
+
+  .list-photos {
+    list-style: none;
+  }
+
+  .list-photos__item {
+    width: auto;
+    display: inline-block;
+    margin: 0px 2px 0px 2px;
+  }
+
+  .list-photos__item-title {
+    display: none;
+  }
 </style>
